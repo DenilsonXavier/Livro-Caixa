1b5fb4264587c546ff174f26258f8eb8fbec5d92
DenilsonXavier <127784165+DenilsonXavier@users.noreply.github.com>
2023-06-01 12:59:27 -0300

    25

--- Controller/Login.php
+++ Controller/Login.php
@@ -1,8 +1,10 @@
 
 <?php
 require_once '../class/Conect.php';
+require_once '../class/Usuario.php';
 class Login extends Conexao {
-    public function validarUsuario($nick = "ted", $senha = "senha123") {
+    public function validarUsuario($nick, $senha) {
+        
         $this->conectar();
 
         $consulta = $this->conexao->prepare("SELECT nivel FROM usuario WHERE nick = ? AND senha = ?");
@@ -16,14 +18,14 @@ class Login extends Conexao {
             $nivel = $row['nivel'];
             
             if ($nivel === 'administrador' || $nivel === 'funcionario') {
-                // Usuário válido com nível de acesso correto
+               
                 $this->fecharConexao();
-                header("Location: index.php");
-                exit();
+                header("Location: ../index.php");
+                return true;
             }
         }
 
-        // Usuário inválido ou nível de acesso incorreto
+       
         $consulta->close();
         $this->fecharConexao();
         return false;
@@ -33,22 +35,19 @@ require_once 'Login.php';
 
 $login = new Login();
 
-// Testando com um usuário válido
-$usuarioValido = $login->validarUsuario("ted", "senha123");
-if ($usuarioValido) {
-    echo "Usuário válido. Redirecionando para index.php...";
-    // Redirecionar para index.php
-} else {
-    echo "Usuário inválido. Por favor, verifique suas credenciais.";
-}
+if (isset($_POST['nick']) && isset($_POST['senha'])) {
+    $nick = $_POST['nick'];
+    $senha = $_POST['senha'];
 
-// Testando com um usuário inválido
-$usuarioInvalido = $login->validarUsuario('joao', 'senha456');
-if ($usuarioInvalido) {
-    echo "Usuário válido. Redirecionando para index.php...";
-    // Redirecionar para index.php
-} else {
-    echo "Usuário inválido. Por favor, verifique suas credenciais.";
+    $usuarioValido = $login->validarUsuario($nick, $senha);
+    if ($usuarioValido) {
+        echo "Usuário válido. Redirecionando para index.php...";
+        
+        header("Location: ../index.php");
+        exit();
+    } else {
+        echo "Usuário inválido. Por favor, verifique suas credenciais.";
+        header("Location:../telaLogin.php?error=invalid");
+     exit();
+    }
 }
-
-?>
---
+++ telaLogin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Tela de Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=25">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <form class="login-form p-5 border rounded shadow" method="post" action="../Livro-Caixa/Controller/Login.php">
+            <h2 class="text-center mb-4">BEM VINDO</h2>
+            <?php
+            if (isset($_GET['error']) && $_GET['error'] === 'invalid') {
+                echo '<p class="text-danger">Nick ou senha incorretos. Por favor, verifique suas credenciais.</p>';
+            }
+            ?>
+            <div class="form-group">
+                <input type="text" name="nick" class="form-control" placeholder="Nick" required>
+            </div>
+            <div class="form-group">
+                <input type="password" name="senha" class="form-control" placeholder="Senha" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Entrar" class="btn btn-success btn-block">
+            </div>
+        </form>
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
